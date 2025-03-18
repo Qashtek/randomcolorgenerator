fe0f29f98ca5ea78fb670ab83b6adc442f7628e9
qashtek <qashtechnologies@gmail.com>
2025-03-18 23:05:47 +0100

    initial deposit

---
+++ random.js
@@ -0,0 +1,23 @@
+
+const btn = document.querySelector('button');
+const h1 = document.querySelector('h1');
+btn.addEventListener('click', () => {
+    
+
+    const r = Math.floor(Math.random() * 255);
+    const g= Math.floor(Math.random() * 255);
+    const b= Math.floor(Math.random() * 255);
+
+    const newColor = `rgb(${r},${g},${b})`;
+    document.body.style.backgroundColor = newColor;
+    h1.textContent = newColor;
+    btn.style.color = newColor;
+
+    const rgbSum = r + g + b;
+    if(rgbSum < 100){
+        document.body.style.color = 'white';
+    }else {
+        document.body.style.color = 'black'
+    }
+    
+})
